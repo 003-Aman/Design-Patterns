3eb5a6949a683b3d21abf4c43391eebbc866e69a
003-Aman <amanshrestha3003@gmail.com>
2024-07-22 23:02:51 -0400

    Design Patterns

---
+++ MomentoPattern.java/Main.java/EditorState.java
@@ -0,0 +1,12 @@
+public class EditorState {
+    private final String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public EditorState(String content){
+        this.content = content;
+    }
+    
+}
